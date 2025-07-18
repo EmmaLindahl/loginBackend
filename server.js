7544c52f63fe66520cf4fa907c6585c3b8096840
Emma Lindahl <emma.sofia.dahl@gmail.com>
2025-07-18 15:37:34 +0200

    changed PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
